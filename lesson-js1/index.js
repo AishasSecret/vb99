bb65c26db82ee489168911b4e63cd42f3d704008
cekmop_npu3 <milanastrunevskaya@gmail.com>
2023-01-08 19:46:19 +0300

    js 1

---
+++ lesson-js1/index.js
@@ -0,0 +1,58 @@
+// let a
+// a = 5
+// console.log(a);
+
+// const b = 5
+// console.log(5);
+
+// const userName = 'Sasha'
+// console.log(`Hello, ${userName}`);
+
+// console.log(`${5*754-64657+83423/209}`);
+
+// let isGreated = 654 > 25
+// alert(isGreated);
+
+// let c = undefined //ничего нет (нет чиселка вообще, но оно может быть)
+// console.log(c)
+// c = 5
+// console.log(c);
+
+// let d = null //нет ничего (есть целое ничего)
+// console.log(d)
+// d = 5
+// console.log(d);
+
+// const pupil = {
+//    class: '10 A', //свойство объекта
+//    sex: 'female',
+//    age: 15,
+//    name: {
+//       firstName: 'Sasha',
+//       secondName: 'Zhitnik'
+//    }
+// }
+// console.log(pupil);
+// pupil.age = 49
+// console.log(pupil);
+// pupil.interests = 'crocheting'
+// console.log(pupil);
+// delete pupil.class
+// console.log(pupil);
+
+let a = 17934
+let b = 3559
+console.log(a+b);
+
+const c = 4
+const d = 37
+console.log(c-d);
+
+const e = 5
+console.log(typeof e);
+console.log(typeof `${e}`);
+console.log(typeof String(e));
+console.log(e + 'hello');
+
+const f = prompt()
+console.log(f+1);
