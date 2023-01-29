946189276498c7c42e9f48e8182d0e02939791e2
AishasSecret <sashazhitnikdr@gmail.com>
2023-01-29 15:09:25 +0300

    home 2

---
+++ homework 2/index.js
@@ -0,0 +1,107 @@
+// const arr = [1, 2, 3, 4]
+// const first = arr[0] * arr[3]
+// const second = arr[1] * arr[2]
+// if (first > second) {
+//     console.log ('first')
+// } else if (first < second) {
+//     console.log('second');
+// } else {
+//     console.log('same');
+// }
+
+
+// const a = 'i love javascript'
+// const arr = a.split('');
+// arr[0] = arr[0].toUpperCase()
+// arr[2] = arr[2].toUpperCase()
+// arr[4] = arr[4].toUpperCase()
+// arr[6] = arr[6].toUpperCase()
+// arr[8] = arr[8].toUpperCase()
+// arr[10] = arr[10].toUpperCase()
+// arr[12] = arr[12].toUpperCase()
+// arr[14] = arr[14].toUpperCase()
+// arr[16] = arr[16].toUpperCase()
+
+// arr[1] = arr[1].toLowerCase()
+// arr[3] = arr[3].toLowerCase()
+// arr[5] = arr[5].toLowerCase()
+// arr[7] = arr[7].toLowerCase()
+// arr[9] = arr[9].toLowerCase()
+// arr[11] = arr[11].toLowerCase()
+// arr[13] = arr[13].toLowerCase()
+// arr[15] = arr[15].toLowerCase()
+
+// console.log(arr);
+
+
+// const min = 875
+// if (min >= 0 && min <= 15) {
+//     console.log('first');
+// } else if (min >= 16 && min <= 30) {
+//     console.log('second');
+// } else if (min >= 31 && min <= 45) {
+//     console.log('second');
+// } else if (min >= 46 && min <= 60) {
+//     console.log('second');
+// } else {
+//     console.log('Please, write number from 0 to 60');
+// }
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+// for (let i = 0; i < arr.length; i++) {
+//     if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else if (i % 3  && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+//     } else {
+//         console.log('нечетные');
+//     }
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//         console.log(arr[i]);
+//     } else {
+//         console.log('четные');
+//     }
+// }
+
+
+// const a = 30
+// const b = 23
+//  if (a > 10 && a < 45 && a !== 23 && b > 4 && b < 26) {
+//     console.log(a*b);
+//  } else {
+//     console.log('false');
+//  }
+
+
+// const arr = []
+// while (arr.length < 10) {
+//     const random = Math.floor(Math.random() * 100) 
+// }
+// console.log(arr);
+
+
+// const a = Math.floor(Math.random() * 10) 
+// const b = 5
+// if (b > a) {
+//     console.log('много');
+// } else if (b < a) {
+//     console.log('мало');
+// } else {
+//     console.log('вы угадали');
+// }
