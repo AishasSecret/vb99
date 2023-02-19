0a736290411c9182e8576fdfc4e032934622e946
cekmop_npu3 <milanastrunevskaya@gmail.com>
2023-02-19 19:38:26 +0300

    js-6-7

--- js-6/game.js
+++ js-6/game.js
@@ -7,17 +7,31 @@ const inpName = document.getElementById('name')
 const toGame = document.getElementById('toGame')
 const toCount = document.getElementById('toCount')
 
-const imgRock = document.getElementById('rock')
+const imgStone = document.getElementById('stone')
 const imgPaper = document.getElementById('paper')
 const imgScissors = document.getElementById('scissors')
+
+const gamesAll = document.getElementById('gameAll')
+const userWin = document.getElementById('userWin')
+const pcWin = document.getElementById('pcWin')
+
+const divMidRes = document.querySelector('.middleResult');
+const spanMidRes = document.querySelector('#middleResult');
+
+const divRes = document.getElementsByClassName('result')
+const spanRes = document.getElementById('result')
+
+let gamesCount = 1
+
 toCount.setAttribute('disabled',true)
 divGame.style.display = 'none'
 divGameCount.style.display = 'none'
+divRes.style.display = 'none'
 
 const showCount = () => {
    divUserName.style.display = 'none'
    divGameCount.style.display = 'block'
-   inpCount.removeEventListene('input', disableName)
+   inpName.removeEventListener('input', disableName)
 }
 
 const disableName = () => {
@@ -32,3 +46,98 @@ toCount.onclick = () => {
 }
 
 inpName.addEventListener('input', disableName)
+
+toGame.onclick = () => {
+   divGameCount.style.display = 'none'
+   divGame.style.display = 'block'
+   gamesCount = inpCount.value;
+}
+
+const arrChoose = ['rock', 'paper', 'scissors']
+
+let countGames = 0
+let pcCounterWin = 0
+let userCounterWin = 0
+
+const gameCounter = (winner, user, pc) => {
+   switch (winner) {
+      case 'user':
+         userCounterWin++
+         break;
+      case 'pc':
+         pcCounterWin++
+         break;
+   }
+   countGames++
+   gamesAll.innerHTML = countGames
+   userWin.innerHTML = userCounterWin
+   pcWin.innerHTML = pcCounterWin
+   spanMidRes.innerHTML = `PC chose ${pc}, you chose ${user}`
+   divMidRes.style.display = 'block'
+
+   setTimeout(() => {
+      divMidRes.style.display = 'none'
+   }, 3000)
+
+   if (countGames === +gamesCount) {
+      if (userCounterWin > pcCounterWin) {
+         spanMidRes.innerHTML = `You win`
+      } else if (userCounterWin < pcCounterWin) {
+         spanMidRes.innerHTML = `You lose`
+      } else {
+         spanMidRes.innerHTML = `Draw`
+      }
+      divMidRes.style.display = 'block'
+      imgPaper.style.display = 'none'
+      imgStone.style.display = 'none'
+      imgScissors.style.display = 'none'
+   }
+}
+
+imgStone.onclick = () => {
+   const user = 'stone'
+   const pc = arrChoose[Math.floor(Math.random() * 3)]
+   switch (pc) {
+      case 'stone':
+         gameCounter(null, user, pc)
+         break;
+      case 'paper':
+         gameCounter('user', user, pc)
+         break;
+      case 'scissors':
+         gameCounter('pc', user, pc)    //победил, наш ход (stone), комп
+         break;
+   }
+}
+
+imgPaper.onclick = () => {
+   const user = 'paper'
+   const pc = arrChoose[Math.floor(Math.random() * 3)]
+   switch (pc) {
+      case 'stone':
+         gameCounter('user', user, pc)
+         break;
+      case 'paper':
+         gameCounter(null, user, pc)
+         break;
+      case 'scissors':
+         gameCounter('pc', user, pc)
+         break;
+   }
+}
+
+imgScissors.onclick = () => {
+   const user = 'scissors'
+   const pc = arrChoose[Math.floor(Math.random() * 3)]
+   switch (pc) {
+      case 'stone':
+         gameCounter('pc', user, pc)
+         break;
+      case 'paper':
+         gameCounter('user', user, pc)
+         break;
+      case 'scissors':
+         gameCounter(null, user, pc)
+         break;
+   }
+}
